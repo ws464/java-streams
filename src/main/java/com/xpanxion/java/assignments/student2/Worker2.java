4b459310c7f89d96725e6547c624481718a4149f
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2022-01-11 11:17:23 -0600

    Clean up code

--- src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
+++ src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
@@ -11,14 +11,13 @@ import java.util.stream.Collectors;
 
 public class Worker2 {
 
-    DataAccess dataAccess = new DataAccess();
     HashMap<Integer, String> depNames = new HashMap<>();
 
     public void ex1() {
         depNames.put(1,"Electronics");
         depNames.put(2,"Food");
 
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName(depNames.get(p.getDepartmentId()));
@@ -31,7 +30,7 @@ public class Worker2 {
     }
 
     public void ex2() {
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName("N/A");
