2bc2d53b697cf51e02559c9747ec512aa6682c4c
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2022-01-11 11:08:16 -0600

    Add sum of prices in food dep

--- src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
+++ src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
@@ -3,22 +3,24 @@ package com.xpanxion.java.assignments.student2;
 import com.xpanxion.java.assignments.DataAccess;
 import com.xpanxion.java.assignments.model.Product;
 
+import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Worker2 {
 
-    DataAccess dataAccess = new DataAccess();
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
     HashMap<Integer, String> depNames = new HashMap<>();
 
     public void ex1() {
         depNames.put(1,"Electronics");
         depNames.put(2,"Food");
 
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName(depNames.get(p.getDepartmentId()));
@@ -31,7 +33,7 @@ public class Worker2 {
     }
 
     public void ex2() {
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName("N/A");
@@ -44,14 +46,26 @@ public class Worker2 {
     }
 
     public void ex3() {
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .filter(p -> p.getPrice() >= 10)
                 .collect(Collectors.toMap(Product::getId,
                         Function.identity()));
 
-        System.out.println("Ex. 2...");
+        System.out.println("Ex. 3...");
         System.out.println(products);
+    }
+
+    public void ex4() {
+        List<Product> products = DataAccess.getProducts();
+        float priceSum = products
+                .stream()
+                .filter(p -> p.getDepartmentId() == 2)
+                .mapToInt(p -> (int) p.getPrice())
+                .sum();
+
+        System.out.println("Ex. 4...");
+        System.out.println(currency.format(priceSum));
     }
 }
 
