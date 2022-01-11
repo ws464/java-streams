46b227585fc26e3192f1d89a4a6e1fc975ec35dd
Kris Norful <krisnorful@gmail.com>
2022-01-11 11:59:23 -0600

    finish ex4

--- src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
+++ src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
@@ -2,8 +2,12 @@ package com.xpanxion.java.assignments.student1;
 
 import com.xpanxion.java.assignments.DataAccess;
 import com.xpanxion.java.assignments.model.Department;
+import com.xpanxion.java.assignments.model.Product;
 
+import java.text.NumberFormat;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -32,5 +36,24 @@ public class Worker1 {
 
         var result = products.stream().filter(p -> p.getPrice() >= 10.0);
         System.out.println(result.collect(Collectors.toList()));
+    }
+
+    public void ex4() {
+        var products = DataAccess.getProducts();
+        Optional<Float> total;
+        NumberFormat fomatter = NumberFormat.getCurrencyInstance();
+        String moneyString;
+
+        Map<Integer, Department> departmentMap = DataAccess.getDepartments().stream()
+                .collect(Collectors.toMap(Department::getId, Function.identity()));
+
+        total = products.stream()
+                .peek(product -> product.setDepartmentName(departmentMap.get(product.getDepartmentId()).getName()))
+                .filter(p -> Objects.equals(p.getDepartmentName(), "Food"))
+                .map(Product::getPrice)
+                .reduce(Float::sum);
+
+        moneyString = fomatter.format(total.get());
+        System.out.println(moneyString);
     }
 }
