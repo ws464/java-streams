29685019d247e2f9673e65ba5d16c9d29f9ceea9
Aaron Weegens <Aaron.Weegens@walmart.com>
2022-01-11 10:16:51 -0600

    Refactor Map

--- src/main/java/com/xpanxion/java/assignments/student5/Worker5.java
+++ src/main/java/com/xpanxion/java/assignments/student5/Worker5.java
@@ -1,24 +1,27 @@
 package com.xpanxion.java.assignments.student5;
 
 import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
 import com.xpanxion.java.assignments.model.Product;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Worker5 {
 
     public void ex1() {
         List<Product> products = DataAccess.getProducts();
-        Map<Integer, String> departmentNames = new HashMap<>();
+        List<Department> departmentList = DataAccess.getDepartments();
 
-        DataAccess.getDepartments().stream()
-                .forEach(department -> departmentNames.put(department.getId(),department.getName()));
+        Map<Integer, Department> departmentMap = departmentList.stream()
+                .collect(Collectors.toMap(Department::getId, Function.identity()));
 
         products.stream().map(product -> {
           Integer departmentId = Integer.valueOf(product.getDepartmentId());
-          product.setDepartmentName(departmentNames.get(departmentId));
+          product.setDepartmentName(departmentMap.get(departmentId).getName());
           return product;
         }).forEach(System.out::println);
     }
