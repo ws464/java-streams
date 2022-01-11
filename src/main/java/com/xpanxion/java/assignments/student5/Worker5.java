cedab2770252f8d2bdf4df0acfbe9424ca3edce1
Aaron Weegens <Aaron.Weegens@walmart.com>
2022-01-11 09:25:19 -0600

    Finish Ex1

--- src/main/java/com/xpanxion/java/assignments/student5/Worker5.java
+++ src/main/java/com/xpanxion/java/assignments/student5/Worker5.java
@@ -1,8 +1,24 @@
 package com.xpanxion.java.assignments.student5;
 
+import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
+import com.xpanxion.java.assignments.model.Product;
+
+import java.util.List;
+
+import static com.xpanxion.java.assignments.DataAccess.getDepartments;
+
 public class Worker5 {
 
     public void ex1() {
-        System.out.println("Ex. 1...");
+        List<Product> products = DataAccess.getProducts();
+        List<Department> departments = getDepartments();
+
+        products.stream().map(product -> {
+          var departmentId = product.getDepartmentId();
+          var departmentName = departments.get(departmentId).getName();
+          product.setDepartmentName(departmentName);
+          return product;
+        }).forEach(System.out::println);
     }
 }
