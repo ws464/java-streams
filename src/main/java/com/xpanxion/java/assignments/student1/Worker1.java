ffe54bc36baade6866009c5e8cc562fe989c9c3f
Kris Norful <krisnorful@gmail.com>
2022-01-11 10:19:00 -0600

    finish ex2

--- src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
+++ src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
@@ -1,25 +1,30 @@
 package com.xpanxion.java.assignments.student1;
 
 import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Worker1 {
 
     public void ex1() {
         var products = DataAccess.getProducts();
-        var departments = DataAccess.getDepartments();
-        products
-                .stream()
-                .map(product -> product)
-                .forEach(p -> {
-                    departments.forEach(d -> {
-                        if (d.getId() == p.getDepartmentId()) {
-                            p.setDepartmentName(d.getName());
-                        }
-                    });
-                });
+        Map<Integer, Department> departmentMap = DataAccess.getDepartments().stream()
+                .collect(Collectors.toMap(Department::getId, Function.identity()));
 
+        products.forEach(p -> p.setDepartmentName(departmentMap.get(p.getDepartmentId()).getName()));
+        System.out.println(products);
+    }
+
+    public void ex2() {
+        var products = DataAccess.getProducts();
+        Map<Integer, Department> departmentMap = DataAccess.getDepartments().stream()
+                .collect(Collectors.toMap(Department::getId, Function.identity()));
+
+        products.forEach(p -> p.setDepartmentName("N/A"));
         System.out.println(products);
     }
 }
