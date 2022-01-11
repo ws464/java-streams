1f98662adb8aeb001dccbb8b85e761f8b166eec9
Michael Mayes <m0m0sxr@homeoffice.wal-mart.com>
2022-01-11 10:05:34 -0600

    commit ex1

--- src/main/java/com/xpanxion/java/assignments/student6/Worker6.java
+++ src/main/java/com/xpanxion/java/assignments/student6/Worker6.java
@@ -1,8 +1,25 @@
 package com.xpanxion.java.assignments.student6;
 
+import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
+import com.xpanxion.java.assignments.model.Product;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Worker6 {
 
     public void ex1() {
-        System.out.println("Ex. 1...");
+        List<Product> productList = DataAccess.getProducts();
+        List<Department> departmentList = DataAccess.getDepartments();
+
+        Map<Integer, Department> departmentMap = departmentList.stream().collect(Collectors.toMap(Department::getId, i -> (Department)i));
+        List<Product> newProductList = productList.stream().map(p -> {
+            p.setDepartmentName(departmentMap.get(p.getDepartmentId()).getName());
+            return p;
+        }).toList();
+
+        System.out.println(newProductList);
     }
 }
