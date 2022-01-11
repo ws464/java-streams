dc10516cfd4a70e34c110ff8d13735c520b922f6
Elizabeth Michael <lisakada@gmail.com>
2022-01-11 11:04:13 -0600

    Finish Ex.1

--- src/main/java/com/xpanxion/java/assignments/student4/Worker4.java
+++ src/main/java/com/xpanxion/java/assignments/student4/Worker4.java
@@ -1,8 +1,23 @@
 package com.xpanxion.java.assignments.student4;
 
+import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Worker4 {
 
     public void ex1() {
-        System.out.println("Ex. 1...");
+        var  proList = DataAccess.getProducts();
+        var depList = DataAccess.getDepartments();
+
+        Map<Integer, Department> depMap = depList.stream().collect(Collectors.toMap(Department::getId, Function.identity()));
+        var result = proList.stream().map(p -> {
+            p.setDepartmentName(depMap.get(p.getDepartmentId()).getName());
+            return p;
+        }).toList();
+        System.out.println(result);
     }
 }
