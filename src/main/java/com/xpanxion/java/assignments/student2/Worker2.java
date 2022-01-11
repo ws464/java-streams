85f287b129259caf8b8bb420a0c99772c236d32c
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2022-01-11 09:48:13 -0600

    Set department names based on id

--- src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
+++ src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
@@ -1,8 +1,25 @@
 package com.xpanxion.java.assignments.student2;
 
+import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Product;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class Worker2 {
 
+    DataAccess dataAccess = new DataAccess();
+    HashMap<Integer, String> depNames = new HashMap<>();
+
     public void ex1() {
+        depNames.put(1,"Electronics");
+        depNames.put(2,"Food");
+
+        List<Product> products = dataAccess.getProducts();
+        products.stream()
+                .forEach(p -> p.setDepartmentName(depNames.get(p.getDepartmentId())));
+
         System.out.println("Ex. 1...");
+        System.out.println(products);
     }
 }
