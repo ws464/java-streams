9e3e4a8ea2c4b07010ced1821d3131feed26499c
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2022-01-11 13:52:50 -0600

    Add map for departments and implementation

--- src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
+++ src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
@@ -1,29 +1,30 @@
 package com.xpanxion.java.assignments.student2;
 
 import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
 import com.xpanxion.java.assignments.model.Product;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Worker2 {
 
-    DataAccess dataAccess = new DataAccess();
     HashMap<Integer, String> depNames = new HashMap<>();
 
     public void ex1() {
-        depNames.put(1,"Electronics");
-        depNames.put(2,"Food");
+        Map<Integer, String> departmentMap = DataAccess.getDepartments()
+                .stream()
+                .collect(Collectors.toMap(Department::getId,
+                        Department::getName));
 
-        Map<Integer, Product> products = dataAccess.getProducts()
+        List<Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
-                    p.setDepartmentName(depNames.get(p.getDepartmentId()));
+                    p.setDepartmentName(departmentMap.get(p.getDepartmentId()));
                     return p;
-                }).collect(Collectors.toMap(Product::getId,
-                        Function.identity()));
+                }).collect(Collectors.toList());
 
         System.out.println("Ex. 1...");
         System.out.println(products);
