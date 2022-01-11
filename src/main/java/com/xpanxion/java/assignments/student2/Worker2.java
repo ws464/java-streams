4094f5af938f7d8d6febba87c8e1ac7cf6909e89
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2022-01-11 11:20:09 -0600

    Clean up code

--- src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
+++ src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
@@ -4,21 +4,19 @@ import com.xpanxion.java.assignments.DataAccess;
 import com.xpanxion.java.assignments.model.Product;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Worker2 {
 
-    DataAccess dataAccess = new DataAccess();
     HashMap<Integer, String> depNames = new HashMap<>();
 
     public void ex1() {
         depNames.put(1,"Electronics");
         depNames.put(2,"Food");
 
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName(depNames.get(p.getDepartmentId()));
@@ -31,7 +29,7 @@ public class Worker2 {
     }
 
     public void ex2() {
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName("N/A");
@@ -44,13 +42,13 @@ public class Worker2 {
     }
 
     public void ex3() {
-        Map<Integer, Product> products = dataAccess.getProducts()
+        Map<Integer, Product> products = DataAccess.getProducts()
                 .stream()
                 .filter(p -> p.getPrice() >= 10)
                 .collect(Collectors.toMap(Product::getId,
                         Function.identity()));
 
-        System.out.println("Ex. 2...");
+        System.out.println("Ex. 3...");
         System.out.println(products);
     }
 }
