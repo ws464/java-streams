910e4eb80907ea16aa5ecd112e43193040fbc249
Kris Norful <krisnorful@gmail.com>
2022-01-11 09:49:41 -0600

    finish ex1

--- src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
+++ src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
@@ -1,8 +1,26 @@
 package com.xpanxion.java.assignments.student1;
 
+import com.xpanxion.java.assignments.DataAccess;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Worker1 {
 
     public void ex1() {
-        System.out.println("Ex. 1...");
+        var products = DataAccess.getProducts();
+        var departments = DataAccess.getDepartments();
+        AtomicInteger productDepartmentId = new AtomicInteger();
+        products
+                .stream()
+                .map(product -> product)
+                .forEach(p -> {
+                    departments.forEach(d -> {
+                        if (d.getId() == p.getDepartmentId()) {
+                            p.setDepartmentName(d.getName());
+                        }
+                    });
+                });
+
+        System.out.println(products);
     }
 }
