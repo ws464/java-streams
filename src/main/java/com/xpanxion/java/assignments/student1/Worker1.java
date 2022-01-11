24e3b60a11addd0964266fdd176af8cde2d810e6
Kris Norful <krisnorful@gmail.com>
2022-01-11 10:10:44 -0600

    convert departments into map

--- src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
+++ src/main/java/com/xpanxion/java/assignments/student1/Worker1.java
@@ -1,25 +1,22 @@
 package com.xpanxion.java.assignments.student1;
 
 import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Worker1 {
 
     public void ex1() {
         var products = DataAccess.getProducts();
-        var departments = DataAccess.getDepartments();
-        products
-                .stream()
-                .map(product -> product)
-                .forEach(p -> {
-                    departments.forEach(d -> {
-                        if (d.getId() == p.getDepartmentId()) {
-                            p.setDepartmentName(d.getName());
-                        }
-                    });
-                });
+        Map<Integer, Department> departmentMap = DataAccess.getDepartments().stream()
+                        .collect(Collectors.toMap(Department::getId, Function.identity()));
 
+        products.stream().forEach(p -> p.setDepartmentName(departmentMap.get(p.getDepartmentId()).getName()));
         System.out.println(products);
     }
 }
