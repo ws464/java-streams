f26933826010401754b02d5ab081d95308e7d886
Austin King <austintriesemail@gmail.com>
2022-01-11 09:09:46 -0600

    Add Department Name to Product

--- src/main/java/com/xpanxion/java/assignments/student7/Worker7.java
+++ src/main/java/com/xpanxion/java/assignments/student7/Worker7.java
@@ -1,8 +1,26 @@
 package com.xpanxion.java.assignments.student7;
 
+import com.xpanxion.java.assignments.DataAccess;
+
+import java.util.stream.Collectors;
+
 public class Worker7 {
 
     public void ex1() {
-        System.out.println("Ex. 1...");
+        var products = DataAccess.getProducts();
+        var departments = DataAccess.getDepartments();
+
+        var correctProducts = products.stream()
+                .map(p -> {
+                    if (p.getDepartmentId() == 1) {
+                        p.setDepartmentName(departments.get(0).getName());
+                    }
+                    if (p.getDepartmentId() == 2) {
+                        p.setDepartmentName(departments.get(1).getName());
+                    }
+                    return p;
+                })
+                .collect(Collectors.toList());
+        System.out.println(correctProducts);
     }
 }
