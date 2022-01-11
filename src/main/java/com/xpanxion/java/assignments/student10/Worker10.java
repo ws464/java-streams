b7c0cdb6edd7d60532f5639e46735a3b4afc2552
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-01-11 10:05:04 -0600

    definitely fix ex1 this time :)

--- src/main/java/com/xpanxion/java/assignments/student10/Worker10.java
+++ src/main/java/com/xpanxion/java/assignments/student10/Worker10.java
@@ -1,22 +1,31 @@
 package com.xpanxion.java.assignments.student10;
 
 import com.xpanxion.java.assignments.DataAccess;
+
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class Worker10 {
 
     public void ex1() {
-        System.out.println("Ex. 1...");
+        System.out.println("Ex. 1");
         var productList = DataAccess.getProducts();
         var departmentList = DataAccess.getDepartments();
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        departmentList.stream()
+                .forEach(department -> {
+                    hashMap.put(department.getId(), department.getName());
+                });
 
-        var newProductList = productList.stream().map(product -> {
-                    var departmentId = product.getDepartmentId() - 1;
-                    var departmentName = departmentList.get(departmentId).getName();
+        var newProductList = productList.stream()
+                .map(product -> {
+                    var departmentId = product.getDepartmentId();
+                    var departmentName = hashMap.get(departmentId);
                     product.setDepartmentName(departmentName);
                     return product;
                 })
                 .collect(Collectors.toList());
+
         System.out.println(newProductList);
     }
 }
