47f955df57b63594a041973a5aeaac7ab86aa83f
Meg <helloiammegg@gmail.com>
2022-01-11 10:45:10 -0600

    finish ex2

--- src/main/java/com/xpanxion/java/assignments/student8/Worker8.java
+++ src/main/java/com/xpanxion/java/assignments/student8/Worker8.java
@@ -3,17 +3,16 @@ package com.xpanxion.java.assignments.student8;
 import com.xpanxion.java.assignments.DataAccess;
 import com.xpanxion.java.assignments.model.Department;
 
+import javax.xml.crypto.Data;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Worker8 {
 
-    DataAccess dataAccess;
-
     public void ex1() {
-        System.out.println("Exercise 1, Student 8:");
+        System.out.println("EXERCISE 1:");
 
-        Map<Integer, String> departmentsMap = dataAccess.getDepartments().stream().collect(Collectors.toMap(Department::getId, Department::getName));
+        Map<Integer, String> departmentsMap = DataAccess.getDepartments().stream().collect(Collectors.toMap(Department::getId, Department::getName));
 
         var prodList = DataAccess.getProducts();
 
@@ -23,8 +22,18 @@ public class Worker8 {
         }).toList();
 
         System.out.println(updatedProdList);
-
     }
 
+    public void ex2() {
+        System.out.println("\nEXERCISE 2:");
 
+        var prodList = DataAccess.getProducts();
+
+        var newProdList = prodList.stream().map(p -> {
+            p.setDepartmentName("N/A");
+            return p;
+        }).toList();
+
+        System.out.println(newProdList);
+    }
 }
