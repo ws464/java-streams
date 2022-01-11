7d70538ca1dec02831cd0c3bc0a22985c872b20c
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2022-01-11 14:30:49 -0600

    Price sum of food section

--- src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
+++ src/main/java/com/xpanxion/java/assignments/student2/Worker2.java
@@ -1,61 +1,56 @@
 package com.xpanxion.java.assignments.student2;
 
 import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
 import com.xpanxion.java.assignments.model.Product;
 
 import java.text.NumberFormat;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Worker2 {
-
-    NumberFormat currency = NumberFormat.getCurrencyInstance();
-    HashMap<Integer, String> depNames = new HashMap<>();
-
     public void ex1() {
-        depNames.put(1,"Electronics");
-        depNames.put(2,"Food");
+        Map<Integer, String> departmentMap = DataAccess.getDepartments()
+                .stream()
+                .collect(Collectors.toMap(Department::getId,
+                        Department::getName));
 
-        Map<Integer, Product> products = DataAccess.getProducts()
+        List<Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
-                    p.setDepartmentName(depNames.get(p.getDepartmentId()));
+                    p.setDepartmentName(departmentMap.get(p.getDepartmentId()));
                     return p;
-                }).collect(Collectors.toMap(Product::getId,
-                        Function.identity()));
+                }).collect(Collectors.toList());
 
         System.out.println("Ex. 1...");
         System.out.println(products);
     }
 
     public void ex2() {
-        Map<Integer, Product> products = DataAccess.getProducts()
+        List<Product> products = DataAccess.getProducts()
                 .stream()
                 .map(p -> {
                     p.setDepartmentName("N/A");
                     return p;
-                }).collect(Collectors.toMap(Product::getId,
-                        Function.identity()));
+                }).collect(Collectors.toList());
 
         System.out.println("Ex. 2...");
         System.out.println(products);
     }
 
     public void ex3() {
-        Map<Integer, Product> products = DataAccess.getProducts()
+        List<Product> products = DataAccess.getProducts()
                 .stream()
                 .filter(p -> p.getPrice() >= 10)
-                .collect(Collectors.toMap(Product::getId,
-                        Function.identity()));
+                .collect(Collectors.toList());
 
         System.out.println("Ex. 3...");
         System.out.println(products);
     }
 
     public void ex4() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
         List<Product> products = DataAccess.getProducts();
         float priceSum = products
                 .stream()
