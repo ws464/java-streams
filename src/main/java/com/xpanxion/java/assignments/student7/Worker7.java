93ac14b131e7a62ac90c48f6d0acc2d032ee5ee4
Austin King <austintriesemail@gmail.com>
2022-01-11 09:59:05 -0600

    Use Map To Find Id

--- src/main/java/com/xpanxion/java/assignments/student7/Worker7.java
+++ src/main/java/com/xpanxion/java/assignments/student7/Worker7.java
@@ -1,7 +1,11 @@
 package com.xpanxion.java.assignments.student7;
 
 import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Worker7 {
@@ -10,9 +14,11 @@ public class Worker7 {
         var products = DataAccess.getProducts();
         var departments = DataAccess.getDepartments();
 
+        Map<Integer, Department> departmentsMap = departments.stream().collect(Collectors.toMap(Department::getId, Function.identity()));
+
         var correctProducts = products.stream()
                 .map(p -> {
-                        p.setDepartmentName(departments.get(p.getDepartmentId()-1).getName());
+                        p.setDepartmentName(departmentsMap.get(p.getDepartmentId()).getName());
                     return p;
                 })
                 .collect(Collectors.toList());
