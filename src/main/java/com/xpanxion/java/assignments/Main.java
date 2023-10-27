4d44d172e627bb2835165b58c373f7c4ccedc951
Dhruv Thota <dhruv@Dhruvs-MacBook-Pro-2.local>
2023-10-27 14:12:39 -0400

    fixing visibility

--- src/main/java/com/xpanxion/java/assignments/Main.java
+++ src/main/java/com/xpanxion/java/assignments/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Worker justALowlyWorker = new Worker();
+
+        // Testing exercise 1
         justALowlyWorker.ex1();
     }
 }
---
+++ src/main/java/com/xpanxion/solution/Worker.java
@@ -0,0 +1,13 @@
+package com.xpanxion.solution;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.xpanxion.java.assignments.DataAccess;
+import com.xpanxion.java.assignments.model.Department;
+
+public class Worker {
+    public void ex1 () {
+        // System.out.println("Start here");
+    }
+}
