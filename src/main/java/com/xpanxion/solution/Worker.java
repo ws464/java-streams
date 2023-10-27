f7ce21a2be84169fc22d3f1038653d91b3c5e200
Sassi <william.sassi@fiserv.com>
2023-10-27 15:57:40 -0400

    ex1

--- src/main/java/com/xpanxion/solution/Worker.java
+++ src/main/java/com/xpanxion/solution/Worker.java
@@ -2,12 +2,21 @@ package com.xpanxion.solution;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.HashMap;
 
 import com.xpanxion.java.assignments.DataAccess;
 import com.xpanxion.java.assignments.model.Department;
+import com.xpanxion.java.assignments.model.Product;
 
 public class Worker {
     public void ex1 () {
-        // System.out.println("Start here");
+        var list = DataAccess.getProducts();
+        var departments = new HashMap<Integer, String>();
+        departments.put(1, "Electronics");
+        departments.put(2, "Food");
+        departments.put(3, "Automotive");
+        departments.put(4, "Home");
+        var newList = list.stream().map(p -> new Product(p.getId(), p.getDepartmentId(), departments.get(p.getDepartmentId()), p.getName(), p.getPrice(), p.getSku())).collect(Collectors.toList());
+        System.out.println(newList.toString());
     }
 }
